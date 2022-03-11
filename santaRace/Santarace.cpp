fb314348ca8086e8ce8257893ee731faa4180d09
Uree1229 <101373376+Uree1229@users.noreply.github.com>
2022-03-11 11:30:14 +0900

    Update Santarace.cpp

--- santaRace/Santarace.cpp
+++ santaRace/Santarace.cpp
@@ -1,20 +1,21 @@
-﻿#include <bangtal.h>
+#include <bangtal.h>
+#include <cstdlib>
 
 TimerID timer1;
 SceneID scene1;
-ObjectID startButton, endButton, playButton, santa;
-int santaX = 0, santaY = 500;
+ObjectID startButton, endButton, santa, cloud;
+
+int santaX = 0, santaY = 500, cloudX, cloudY;
 
 
 void endGame(bool success) {
 
 	if (success == true) {
-		showMessage("succes");
+		showMessage("배달에 성공했습니다!");
 	}
 	else {
-		showMessage("miss");
+		showMessage("배달을 실패했습니다 ㅠㅠ");
 	}
-	hideObject(playButton);
 
 	setObjectImage(startButton, "Images/restart.png");
 	showObject(startButton);
@@ -28,9 +29,9 @@ void startGame() {
 	hideObject(startButton);
 	hideObject(endButton);
 
-	showObject(playButton);
+	showMessage("방향키를 사용하여 산타를 움직을 수 있습니다!");
 
-	setTimer(timer1, 10.0f);
+	setTimer(timer1, 30.0f);
 	startTimer(timer1);
 }
 void mouse_Callback(ObjectID object, int x, int y, MouseAction action) {
@@ -39,18 +40,40 @@ void mouse_Callback(ObjectID object, int x, int y, MouseAction action) {
 		endGame();
 	
 	}else if (object == startButton){
-		void startGame();
-		
+		 startGame();
+	}
+}
+void keybord_Callback(KeyCode key, KeyState state) { 
+
+	if (state == KeyState::KEY_PRESSED) {
+		if (key == KeyCode::KEY_UP_ARROW) {
+			santaY = santaY + 20;
+		}
+		else if (key == KeyCode::KEY_DOWN_ARROW) {
+			santaY = santaY - 20;
+		}
+		else if (key == KeyCode::KEY_RIGHT_ARROW) {
+			santaX = santaX + 20;
+		}
+		else if (key == KeyCode::KEY_LEFT_ARROW) {
+			santaX = santaX - 20;
+		}
+	}
 	
-	}else if (object == playButton) {
-		
-		santaX = santaX + 30;
-		locateObject(santa, scene1, santaX, santaY);
-		
-		if (santaX > 1280) {
-			
-			endGame(true);
-		 }
+	locateObject(santa, scene1, santaX, santaY);
+
+	if (santaX > 1280) {
+		endGame(true);
+	}else if (santaX < 0) {
+		showMessage("반대로 가야합니다!");
+		santaX = 0;
+
+	}else if (santaY > 650) {
+		showMessage("고도가 너무 높습니다!");
+		santaY = 650;
+	}else if (santaY < 300) {
+		showMessage("고도가 너무 낮습니다!");
+		santaY = 300;
 	}
 }
 void timer_Callback(TimerID timer) {
@@ -75,7 +98,7 @@ ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown
 }
 
 int main(){
-	
+	setKeyboardCallback(keybord_Callback);
 	setMouseCallback(mouse_Callback);
 	setTimerCallback(timer_Callback);
 
@@ -83,10 +106,11 @@ int main(){
 
 	startButton = createObject("Images/start.png", scene1, 590, 70, true);
 	endButton = createObject("Images/end.png", scene1, 590, 20, true);
-	playButton = createObject("Images/play.png", scene1, 610, 30, false);
+	cloud = createObject("Images/cloud.png",scene1, cloudX, cloudY, true);
 	santa = createObject("Images/santa.png", scene1, santaX, santaY, true);
+	scaleObject(santa, 0.5f);
 
-	timer1 = createTimer(10.0f);
+	timer1 = createTimer(30.0f);
 	showTimer(timer1);
 
 	startGame(scene1);
